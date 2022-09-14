9f14a29f19b25987584a8dd04fd32bb975e2a5bf
viktor <viktorvanyatanya@gmail.com>
2022-09-14 18:03:51 +0200

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <button @click="isVisible = !isVisible">toggle list visibility</button>
+  <Test v-if="isVisible" :items="items" @removeItem="removeItemFromList" :text="userName"/>
+  <h1 v-else-if="!isVisible && counter === 0">some strange condition</h1>
+  <h1 v-else>List is hidden</h1>
+  <div v-show="isVisible">{{user.name}}</div>
+  <div>counter value: {{counter}}</div>
+  <button v-on:click="incCounter">inc</button>
+
+<!--  <h3>{{items[0].text}} <button @click="removeItem(items[0].id)">remove</button></h3>-->
+<!--  <h3>{{items].text}} <button @click="removeItem(items[1].id)">remove</button></h3>-->
+<!--  <h3>{{items[2].text}} <button @click="removeItem(items[2].id)">remove</button></h3>-->
+
+<!--  <h3 v-for="item of items" :key="item.id">-->
+<!--    {{item.text}} <button @click="removeItem(item.id)">remove</button>-->
+<!--  </h3>-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Test from './components/Test.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isVisible: true,
+      items: [{id: 1, text: 'text1'}, {id: 2, text: 'text2'},{id: 3, text: 'text3'}],
+      counter: 0,
+      userName: 'Taras',
+      user: {
+        name: 'Vik'
+      }
+    }
+  },
+  methods: {
+    incCounter() {
+      this.counter++;
+      this.user.name+='!';
+    },
+    removeItemFromList(id) {
+      this.items = this.items.filter(item => item.id !== id);
+    },
+    toggleListVisibility() {
+
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld,
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>test component {{text}}</h1>
+  <button @click="logger">log</button>
+  <h3 v-for="item of items" :key="item.id">
+    {{item.text}} <button @click="removeItem(item.id)">remove</button>
+  </h3>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Test',
+  // props: ['text'],
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    logger() {
+      // this;
+    },
+    removeItem(id) {
+      this.$emit('removeItem', id)
+      // this.items = this.items.filter(item => item.id !== id);
+    }
+  },
+  emits: ['removeItem']
+}
+</script>
+
+<style scoped>
+
+</style>
